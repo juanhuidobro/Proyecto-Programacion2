410e245cc20c75d22b15963bf4da1cde998a5c23
joacopazos <jpazospesquero@udesa.edu.ar>
2020-09-25 19:00:27 -0300

    instale dependencia y comente detallepost en rutas

--- routes/rutas.js
+++ routes/rutas.js
@@ -6,7 +6,7 @@ let controlador = require("../controller/controlador")
 router.get('/home', controlador.home);
 
 router.get('/agregarPost', controlador.agregarPost);
-router.get('/detallePost', controlador.detallePost);
+//router.get('/detallePost', controlador.detallePost);
 router.get('/detalleUsuario', controlador.detalleUsuario);
 router.get('/login', controlador.login);
 router.get('/miPerfil', controlador.miPerfil);
