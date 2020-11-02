4212431812d7229340d27f366f37ace3b3cadc12
joacopazos <jpazospesquero@udesa.edu.ar>
2020-11-02 15:13:26 -0300

    logout

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const usersControlador = require('../controller/usersControlador');
 var router = express.Router();
+var usersController = require('../controller/usersControlador')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/logout', usersControlador.logout)
 
 module.exports = router;
